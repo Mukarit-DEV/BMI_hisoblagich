d41dabcaaa8b85e6ac93b0bb09af1d6e00f88406
Mukarit-DEV <84167812+Mukarit-DEV@users.noreply.github.com>
2021-05-17 21:13:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,20 @@ function bmi() {
     const height = parseFloat(height1.value);
 
     const bmi = (weight / (height * height)).toFixed(2);
+    
+    const a = "ozg'in";
+    const b = "o'rtacha";
+    const c = "semiz";
+    const d = "juda semiz"
+    
+    if (bmi < 18.5){
+        natija.textContent = (`${ism} ${familiya} ning BMI si ${bmi}. Siz ${a}siz. Semiring !!!`);
+    } else if (bmi < 24.9) {
+        natija.textContent = (`${ism} ${familiya} ning BMI si ${bmi}. Siz ${b}siz. Shunday vaznda qoling !!!`);
+    } else if (bmi < 29.9) {
+        natija.textContent = (`${ism} ${familiya} ning BMI si ${bmi}. Siz ${c}siz. Ozroq vazn tashlang !!!`);
+    } else {
+        natija.textContent = (`${ism} ${familiya} ning BMI si ${bmi}. Siz ${d}siz. Siz parxez qilishingiz zarur !!!`);
+    
     natija.textContent = (`${ism} ${familiya} ning BMI si ${bmi}`);
 }
